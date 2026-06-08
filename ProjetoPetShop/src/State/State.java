dc119d3583855fef1ff8d7719f2ec8acc159ac39
igorg <igorg@ArchLinux>
2026-06-08 12:12:39 -0300

    Feat: Adicionado um pacote para o state e a inicialização do codigo do state para o agendamento

---
+++ ProjetoPetShop/src/State/State.java
@@ -0,0 +1,26 @@
+package State;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author igorg
+ */
+public class State {
+    Agendamento agendar();
+    
+    State(Agendamento agendar){
+        
+        this agendar = agendar;
+    }
+    
+    public String Concluir();
+    public String Cancelar();
+    public String Pendente();
+    public String Confirmado();
+    public String Reagendar();
+}
