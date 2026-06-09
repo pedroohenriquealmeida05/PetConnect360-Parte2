0a8a4b28dae0c836a3c324b96eaafa2cc0525299
igorg <igorg@ArchLinux>
2026-06-09 14:36:19 -0300

    Test: Adicionado o junity teste para o state

---
+++ ProjetoPetShop/test/JUnityState/StateJunityTeste.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package JUnityState;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author igorg
+ */
+public class StateJunityTeste {
+    
+    public StateJunityTeste() {
+    }
+    
+   
+
+   @Test
+    public void hello() {}
+}
