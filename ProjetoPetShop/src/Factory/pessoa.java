f941c20c9102ee6eeeaf70b5d144e965d8770640
Felipe <felipefcapucho@gmail.com>
2026-06-11 14:50:04 -0300

    fix: editar classe pessoa

--- ProjetoPetShop/src/Factory/pessoa.java
+++ ProjetoPetShop/src/Factory/pessoa.java
@@ -1,12 +1,30 @@
 package Factory;
 
 
-public class pessoa {
-
-    protected int id;
+/**
+ * Classe abstrata que representa o produto base no padrão Factory Method.
+ * Define os atributos e comportamentos comuns a todos os usuários do PetConnect 360.
+ */
+public abstract class pessoa {
     protected String nome;
-    protected String telefone;
     protected String documento;
-    protected String endereco;
 
+    /**
+     * Construtor base para a criação de uma pessoa.
+     * @param nome Nome do usuário.
+     * @param documento Documento de identificação (CPF ou CNPJ).
+     */
+    public pessoa(String nome, String documento) {
+        this.nome = nome;
+        this.documento = documento;
+    }
+
+    public String getNome() { return nome; }
+    public String getDocumento() { return documento; }
+    
+    /**
+     * Método abstrato que cada subclasse implementará para exibir seus detalhes específicos.
+     * @return String com os dados formatados do usuário.
+     */
+    public abstract String exibirDetalhes();
 }
