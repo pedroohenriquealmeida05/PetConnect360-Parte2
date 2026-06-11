16d451958246010212205f352d517aa034a3f3ef
Felipe <felipefcapucho@gmail.com>
2026-06-11 14:54:50 -0300

    fix: editar classe Tutor

--- ProjetoPetShop/src/Factory/Tutor.java
+++ ProjetoPetShop/src/Factory/Tutor.java
@@ -1,12 +1,17 @@
 package Factory;
 
 
-import Factory.pessoa;
-
-
+/**
+ * Produto Concreto: Representa o cliente do Pet Shop / Clínica.
+ */
 public class Tutor extends pessoa {
-
-    public Tutor() {
+    
+    public Tutor(String nome, String documento) {
+        super(nome, documento);
     }
 
+    @Override
+    public String exibirDetalhes() {
+        return "Tutor: " + this.nome + " | CPF: " + this.documento;
+    }
 }
